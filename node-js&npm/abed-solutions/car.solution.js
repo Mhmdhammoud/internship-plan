bb46efa997b7e584cc463e308d4e91339595f177
Abed <abedbeydounaccs@gmail.com>
2024-08-19 01:41:40 +0300

    Created class and constructor

---
+++ node-js&npm/abed-solutions/car.solution.js
@@ -0,0 +1,35 @@
+class Car {
+  constructor(
+    locked,
+    numberOfWheels,
+    numberOfSeats,
+    numberOfGears,
+    isHatchBack,
+    engineType,
+    engineOn,
+    hasAc,
+    acOn,
+    fuelLeft,
+    tankSize,
+    lightsOn,
+    model,
+    brand,
+    tirePressure
+  ) {
+    this.locked = locked;
+    this.numberOfWheels = numberOfWheels;
+    this.numberOfSeats = numberOfSeats;
+    this.numberOfGears = numberOfGears;
+    this.isHatchBack = isHatchBack;
+    this.engineType = engineType;
+    this.engineOn = engineOn;
+    this.hasAc = hasAc;
+    this.acOn = acOn;
+    this.fuelLeft = fuelLeft;
+    this.tankSize = tankSize;
+    this.lightsOn = lightsOn;
+    this.model = model;
+    this.brand = brand;
+    this.tirePressure = tirePressure;
+  }
+}
