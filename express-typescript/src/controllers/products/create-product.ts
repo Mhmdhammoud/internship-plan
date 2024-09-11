85cd9b64d1459053c59a2f4385ddae83ff2a1166
Mohamamd Hammoud <mohammad.hammoud.lb@hotmail.com>
2024-09-11 21:55:16 +0300

    Added custom body to express typescript

--- express-typescript/src/controllers/products/create-product.ts
+++ express-typescript/src/controllers/products/create-product.ts
@@ -1,15 +1,22 @@
 import { Response } from 'express'
-import { ICreateProduct, OurRequest } from '../../types'
+import { ICreateProduct, IProduct, OurRequest } from '../../types'
+import { OurResponse } from '../../types/request'
 
-export default async (req: OurRequest<ICreateProduct>, res: Response) => {
+export default async (
+    req: OurRequest<ICreateProduct>,
+    res: OurResponse<IProduct | null>
+) => {
     try {
         return res.status(200).json({
-            message: 'Success'
+            status: 'Success',
+            message: 'Product was created successfully.',
+            data: null
         })
     } catch (err) {
         const error = err as Error
         console.log(error.message)
         return res.status(500).json({
+            status: 'Failure',
             message: 'Internal Server Error'
         })
     }
--- express-typescript/src/types/request.ts
+++ express-typescript/src/types/request.ts
@@ -1,5 +1,14 @@
-import { Request } from 'express'
+import { Request, Response } from 'express'
 
 export interface OurRequest<T> extends Request {
     body: T
+}
+export interface OurResponse<T> extends Response {
+    json: (body?: MyJson<T>) => this
+}
+
+type MyJson<T> = {
+    status: 'Success' | 'Failure'
+    message: string
+    data?: T
 }
